d08b65abcfdf881a26216dd9c93a01968abe9bb9
Rodolfo Oliveira <rodolfo.oliveira.rod@gmail.com>
2021-07-09 17:21:02 -0300

    Cadastro de usuario - aula 2

---
+++ Cadastro_Usuario_Ex1/Menu.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Cadastro_Usuario_Ex1
+{
+    class Menu
+    {
+        public static void MontaMenu()
+        {
+            //cabeçalho
+            Cabeçalho();
+            //opções
+            opcoes();
+            //rodape
+            rodape();
+            //escoha
+            escolha();
+        }
+
+        private static void escolha()
+        {
+            int opcao = 0;
+            do
+            {
+                opcao = Validacao.ValidaInteiros();
+                switch (opcao)
+                {
+                    case 0:
+                        Console.WriteLine("Tchau\nAperte qualquer botão para sair ");
+                        Console.ReadLine();
+                        break;
+                    case 1:
+                        string resposta = "";
+                        List<Usuario> usuarios = new List<Usuario>();
+                        do
+                        {
+                            Usuario user = new Usuario();
+                            user.Cadastro();
+                            Console.WriteLine("Deseja cadastrar mais 1?(y/n)");
+                            resposta = Console.ReadLine();
+
+                            usuarios.Add(user);
+                        } while (resposta == "y");
+
+                        Usuario userSelect1 = usuarios.FirstOrDefault(u => u.nome.Equals("Chimbinha"));
+                        Usuario userSelect2 = (from u in usuarios where u.nome == "Chimbinha" select u).FirstOrDefault();
+                        
+                        foreach (var item in usuarios)
+                        {
+                            Console.WriteLine($"Seus dados são:\n nome: {item.nome.ToLower()}\n sobrenome: {item.sobrenome.ToLower()}\n idade: {item.idade}\n email: {item.email.ToLower()}\n endereço: {item.endereco.ToLower()}\n");
+                        }
+                        break;
+                    case 2:
+                        Console.WriteLine("Em implementação (falta dev)\nAperte qualquer botão para sair");
+                        Console.ReadLine();
+                        break;
+                    default:
+                        Console.WriteLine("Opção invalida! Digite 0,1,2");
+                        break;
+
+                }
+            } while (opcao < 0 || opcao > 2);
+
+        }
+
+        private static void rodape()
+        {
+            Console.WriteLine("\n********************************************");
+            Console.WriteLine("Escolha um das opções no menu.");
+            Console.WriteLine("********************************************");
+        }
+
+        private static void opcoes()
+        {
+
+            Console.WriteLine("1-Cadastrar Usuário");
+            Console.WriteLine("2-Editar Usuário");
+            Console.WriteLine("0-Sair do programa");
+        }
+
+        private static void Cabeçalho()
+        {
+            Console.WriteLine("**************** Havan Labs ****************");
+            Console.WriteLine("**************** Bem vindo *****************\n");
+        }
+    }
+}
---
+++ Cadastro_Usuario_Ex1/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Data;
+
+namespace Cadastro_Usuario_Ex1
+{
+    class Usuario
+    {
+        public string nome = String.Empty;
+        public string sobrenome = String.Empty;
+        public int idade = 0;
+        public string email = String.Empty;
+        public string endereco = String.Empty;
+        public void Cadastro()
+        {
+            Console.WriteLine("Digite seu nome");          
+            this.nome = Validacao.ValidaStringEmBranco();
+            Console.WriteLine("Digite seu sobrenome");
+            this.sobrenome = Validacao.ValidaStringEmBranco();
+            Console.WriteLine("Digite sua idade");
+            this.idade = Validacao.ValidaInteiros();
+            Console.WriteLine("Digite seu e-mail");
+            this.email = Validacao.ValidaStringEmBranco();
+            Console.WriteLine("digite seu endereço");
+            this.endereco = Validacao.ValidaStringEmBranco();
+            Console.WriteLine($"Seus dados são:\n nome: {this.nome.ToLower()}\n sobrenome: {this.sobrenome.ToLower()}\n idade: {this.idade}\n email: {this.email.ToLower()}\n endereço: {this.endereco.ToLower()}\n");
+            Console.WriteLine("aperte qualquer tecla para sair");
+            Console.ReadLine();
+        }
+
+        
+
+     }
+    
+}
